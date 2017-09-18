6a696d2772c3dc6a724f4fe09737350e53289d0c
V d M <vhmoura@gmail.com>
2017-09-18 18:35:12 -0300

    added geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,20 @@ jQuery("#message-form").on('submit', function(e) {
 
    });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('geolocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+       socket.emit('createLocationMessage', {
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude
+       })
+    }, function(){
+        alert('unable to fetch location');
+    });
+
+});
