00388750cfe03cc056c8a328286373969e833b00
V d M <vhmoura@gmail.com>
2017-09-18 21:51:41 -0300

    set current geolocation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 const {generateMessage} = require('./utils/message');
+const {generateLocationMessage}= require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -27,7 +28,7 @@ io.on('connection', (socket) => {
    });   
 
    socket.on('createLocationMessage', (coords) => {
-        io.emit('newMessage', generateMessage('admin', `${coords.latitude}, ${coords.longitude}`))  
+        io.emit('newLocationMessage', generateLocationMessage('admin', coords.latitude, coords.longitude))  
    });
 
    socket.on('disconnect', () => {
